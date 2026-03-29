9cb10b58408e990dd13728c950763c11d3255053
alexan lima <alexanlima1.0@gmail.com>
2026-03-29 16:52:04 -0300

    Criando a Barra de navegação, criando o Hero Section da pagina, adicionando ccs normalized para limpar o pre estilização.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MainScreen } from "./component/main-screen/main-screen";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [MainScreen],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
---
+++ src/app/component/main-screen/main-screen.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-main-screen',
+  imports: [],
+  templateUrl: './main-screen.html',
+  styleUrls: ['./main-screen.scss'],
+})
+export class MainScreen {
+
+}
