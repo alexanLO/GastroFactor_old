99867576b3dd9f70e9ade0a65340043b49b6eab1
alexan lima <alexanlima1.0@gmail.com>
2026-05-04 22:02:48 -0300

    refactor: refatorando o footer para ser global.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MainScreen } from "./pages/main-screen/main-screen";
+import { FooterComponent } from "./component/footer-component/footer-component";
 
 @Component({
   standalone: true,
   selector: 'app-root',
-  imports: [MainScreen],
+  imports: [MainScreen, FooterComponent],
   templateUrl: './app.html',
   styleUrls: ['./app.scss']
 })
