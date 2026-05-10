c3f71d559464c19bd2b9afac47faa0fb5e36b996
alexan lima <alexanlima1.0@gmail.com>
2026-05-10 03:30:00 -0300

    feat: adicionando url's das paginas do site.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { LoginScreen } from './pages/login-screen/login-screen';
+import { MainScreen } from './pages/main-screen/main-screen';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'grastrofactor', pathMatch: 'full' },
+  { path: 'grastrofactor', component: MainScreen },
+  { path: 'login', component: LoginScreen },
+  {path: 'cadastrar', component: MainScreen} //TODO modificar para a tela de cadastrar
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,14 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { MainScreen } from "./pages/main-screen/main-screen";
-import { FooterComponent } from "./component/footer-component/footer-component";
 
 @Component({
   standalone: true,
   selector: 'app-root',
-  imports: [MainScreen, FooterComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrls: ['./app.scss']
+  styleUrls: ['./app.scss'],
 })
 export class App {
   protected readonly title = signal('GastroFactor');
